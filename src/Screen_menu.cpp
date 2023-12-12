5713c3cb60720ac702b3ad9014d5f4114a792f80
lisalylo <152969988+lisalylo@users.noreply.github.com>
2023-12-12 10:17:10 +0100

    updates

---
+++ src/Screen_menu.cpp
@@ -0,0 +1,5 @@
+#include "raylib.h"
+
+void screen_menu() {
+    DrawText("das ist der menu screen state", 10, 10, 30, LIGHTGRAY);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 
 #include "config.h"
 
+#include "Screen_menu.h"
+
+int globalgamestate = 0;
+
 int main() {
     // Raylib initialization
     // Project name, screen size, fullscreen mode etc. can be specified in the config.h.in file
@@ -27,12 +31,39 @@ int main() {
         // ...
 
         BeginDrawing();
-            // You can draw on the screen between BeginDrawing() and EndDrawing()
-            // ...
-            // ...
-            ClearBackground(WHITE);
-            DrawText("Hello, world!", 10, 10, 30, LIGHTGRAY);
-            DrawTexture(myTexture, 10, 100, WHITE);
+        // You can draw on the screen between BeginDrawing() and EndDrawing()
+        //die abfrage ist schlecht, weil wenn 0 gedrückt ist, macht es keinen SInn danach noch nach 1 und 2 zu prüfen
+       if (IsKeyReleased(KEY_KP_0)){
+           globalgamestate = 0;
+       }
+        if (IsKeyReleased(KEY_KP_1)){
+            globalgamestate = 1;
+        }
+        if (IsKeyReleased(KEY_KP_2)){
+            globalgamestate = 2;
+        }
+
+        ClearBackground(WHITE);
+
+        switch (globalgamestate){
+            case 0 :
+                screen_menu();
+            break;
+            case 1 :
+                DrawText("das ist der game screen state", 10, 10, 30, LIGHTGRAY);
+            break;
+            case 2 :
+                DrawText("das ist der game over screen state", 10, 10, 30, LIGHTGRAY);
+            default:
+                break;
+        }
+
+        if (globalgamestate == 0) {
+        DrawText("das ist der menu screen state", 10, 10, 30, LIGHTGRAY);
+    }else{
+        DrawText("das ist was anderes", 10, 10, 30, LIGHTGRAY);
+        }
+        DrawTexture(myTexture, 10, 100, WHITE);
 
         EndDrawing();
     } // Main game loop end
