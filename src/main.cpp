af85c60eee9dda4f23a135700be218473d2dace6
Alex <147644122+Alex23052001@users.noreply.github.com>
2023-12-12 10:53:08 +0100

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,14 @@
 
 #include "config.h"
 
+#include "screen_menu.h"
+
+#include "screen_game.h"
+
+#include "screen_gameover.h"
+
+int globalgamestate = 0;
+
 int main() {
     // Raylib initialization
     // Project name, screen size, fullscreen mode etc. can be specified in the config.h.in file
@@ -25,12 +33,43 @@ int main() {
         // Updates that are made by frame are coded here
         // ...
         // ...
+        if (IsKeyReleased(KEY_ONE)){
+            globalgamestate = 0;
+        }
+        if (IsKeyReleased(KEY_TWO)){
+            globalgamestate = 1;
+        }
+        if (IsKeyReleased(KEY_THREE)){
+            globalgamestate = 2;
+        }
 
         BeginDrawing();
             // You can draw on the screen between BeginDrawing() and EndDrawing()
             // ...
             // ...
             ClearBackground(WHITE);
+
+
+        switch (globalgamestate) {
+            case 0:
+               screen_menu();
+               break;
+                break;
+            case 1:
+                screen_game();
+                break;
+            case 2:
+               screen_gameover();
+               break;
+        }
+            //if(globalgamestate == 0){
+               // DrawText("Das ist der Menu Screen-state", 10, 10, 30, LIGHTGRAY);
+           // }
+            //else{
+            //    DrawText("Das ist ein anderer State",10,10,30,LIGHTGRAY);
+           // }
+
+
             DrawText("Hello, world!", 10, 10, 30, LIGHTGRAY);
             DrawTexture(myTexture, 10, 100, WHITE);
 
---
+++ src/screen_menu.cpp
@@ -0,0 +1,9 @@
+#include <cstdlib>
+
+#include "raylib.h"
+
+#include "config.h"
+
+void screen_menu(){
+    DrawText("Das ist der Menu Screen-state", 10, 10, 30, LIGHTGRAY);
+}
