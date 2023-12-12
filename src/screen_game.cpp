ec418f4d6cdb1064e82b6c63faed3d05fee45688
Alex <147644122+Alex23052001@users.noreply.github.com>
2023-12-12 11:38:15 +0100

    Update

---
+++ src/screen_game.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+
+#include "raylib.h"
+
+#include "config.h"
+
+void screen_game(){
+
+    DrawText("COKIEEEEE", 10, 10, 30, PURPLE);
+    DrawText("Press ENTER to start game", 10, 41, 30, PINK);
+
+}
